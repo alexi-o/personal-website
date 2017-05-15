ccc070083f6b7861d580ce310db7cd6fede3df34
AlexiOHearn <alexiohearn@o.local>
2017-05-15 16:27:14 -0600

    animates logos

--- js/grayscale.js
+++ js/grayscale.js
@@ -1,5 +1,34 @@
 
+(function(){
+    console.log("Flipping");
+    var cards = document.querySelectorAll(".card.effect_random");
+    var timeMin = 1;
+    var timeMax = 10;
+    var timeOuts = [];
 
+    for( var i = 0; i < cards.length; i++){
+        var card = cards[i];
+        var cardId = card.getAttribute("data-id");
+        var id = "timeoutID" + cardId;
+        var time = randomNum(timeMin, timeMax) * 1000;
+        cardsTimeout(id, time, card);
+    }
+    function cardsTimeout(id, time, card) {
+        if(id in timeOuts) {
+            clearTimeout(timeOuts[id]);
+        }
+        timeOuts[id] = setTimeout(function(){
+            var c = card.classList;
+            var newTime = randomNum(timeMin, timeMax) * 1000;
+            c.contains("flipped") === true ? c.remove("flipped") : c.add("flipped");
+            cardsTimeout(id, time, card);
+        }, time);
+    }
+    function randomNum(min, max) {
+        return Math.random() * (max-min) + min;
+    }
+
+})();
 
 // jQuery to collapse the navbar on scroll
 function collapseNavbar() {
