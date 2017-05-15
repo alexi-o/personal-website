e6a74f351189f6702b77ae9c1ac948eaa76a0d16
AlexiOHearn <alexiohearn@o.local>
2017-05-15 17:12:31 -0600

    cleans up divs

--- js/grayscale.js
+++ js/grayscale.js
@@ -1,6 +1,5 @@
 
 (function(){
-    console.log("Flipping");
     var cards = document.querySelectorAll(".card.effect_random");
     var timeMin = 1;
     var timeMax = 10;
@@ -27,7 +26,6 @@
     function randomNum(min, max) {
         return Math.random() * (max-min) + min;
     }
-
 })();
 
 // jQuery to collapse the navbar on scroll
@@ -42,47 +40,41 @@ function collapseNavbar() {
 $(window).scroll(collapseNavbar);
 $(document).ready(collapseNavbar);
 
-// jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
     $('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
         $('html, body').stop().animate({
             scrollTop: $($anchor.attr('href')).offset().top
-        }, 1500, 'easeInOutExpo');
+        }, 1500, 'easeInOutCubic');
         event.preventDefault();
     });
 });
 
-// Closes the Responsive Menu on Menu Item Click
 $('.navbar-collapse ul li a').click(function() {
     $(".navbar-collapse").collapse('hide');
 });
 
-// Google Maps Scripts
 var map = null;
-// When the window has finished loading create our google map below
+
 google.maps.event.addDomListener(window, 'load', init);
 google.maps.event.addDomListener(window, 'resize', function() {
     map.setCenter(new google.maps.LatLng(39.74, -104.99));
 });
 
 function init() {
-    // Basic options for a simple Google Map
-    // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
+
     var mapOptions = {
-        // How zoomed in you want the map to start at (always required)
         zoom: 12,
 
-        // The latitude and longitude to center the map (always required)
-        center: new google.maps.LatLng(39.74, -104.99), // New York
+        // The latitude and longitude to center the map 
+        center: new google.maps.LatLng(39.74, -104.99),
 
         // Disables the default Google Maps UI components
         disableDefaultUI: true,
         scrollwheel: false,
         draggable: false,
 
-        // How you would like to style the map.
-        // This is where you would paste any style found on Snazzy Maps.
+
         styles: [{
             "featureType": "water",
             "elementType": "geometry",
@@ -193,14 +185,10 @@ function init() {
         }]
     };
 
-    // Get the HTML DOM element that will contain your map
-    // We are using a div with id="map" seen below in the <body>
     var mapElement = document.getElementById('map');
 
-    // Create the Google Map using out element and options defined above
     map = new google.maps.Map(mapElement, mapOptions);
 
-    // Custom Map Marker Icon - Customize the map-marker.png file to customize your icon
     var image = 'img/pengi.png';
     var myLatLng = new google.maps.LatLng(39.74, -104.99);
     var beachMarker = new google.maps.Marker({
